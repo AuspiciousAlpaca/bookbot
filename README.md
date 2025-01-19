f904b852d568c6136b2b768133643c5653c9f557
AuspiciousAlpaca <sam@plantfriends.xyz>
2025-01-19 21:59:23 +0100

    Updated Bookbot to output a file instead of printing it to the terminal, updated readme accordingly

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Welcome to BookBot!
 
-This is a bot that returns an ordered list of the amount of words and individual alphabetic characters to your terminal.
-To use, insert the text file's path into the "Book_path" variable and run the code.
+This is a bot that returns an ordered list of the amount of words and individual alphabetic characters to a text file in the reports folder.
+To use, insert the text file's path into the "file_path" variable and run the code.
 
 have fun!
--- main.py
+++ main.py
@@ -1,48 +1,51 @@
 def main():
-    book_path = ""
-    content = get_book_text(book_path)
+    file_path = "books/frankenstein.txt"
+    content = get_text(file_path)
     word_count = get_word_count(content)
-    all_characters = get_character_count(content)
-    alphas = get_only_alphas(all_characters)
-    character_list = dict_to_list(alphas)
-    report = print_report(book_path, word_count, character_list)
+    alpha_characters = get_character_count(content)
+    character_list = dict_to_list(alpha_characters)
+    report = return_report(file_path, word_count, character_list)
     return report
 
-def get_book_text(book_path):
-    with open(book_path) as f:
+# grabs the file's content as a string
+def get_text(file_path):
+    with open(file_path) as f:
         return f.read()
 
+# splits the content into words and counts them
 def get_word_count(content):
     words = content.split()
     return len(words)
 
+# checks for alphabet characters in the content and adds them to a dictionary
 def get_character_count(content):
     lowered = content.lower()
     characters = dict()
     for character in lowered:
-        if character not in characters:
-            characters[character] = 1
-        else:
-            characters[character] += 1
+        if character.isalpha():
+            if character not in characters:
+                characters[character] = 1
+            else:
+                characters[character] += 1
     return characters
 
-def get_only_alphas(all_characters):
-    only_alphas = {key: value for key, value in all_characters.items() if key.isalpha()}
-    return only_alphas
-
+# sorting logic
 def sort_on(dict):
     return dict["count"]
 
-def dict_to_list(alphas):
-    character_list = [{"character": character, "count": count} for character, count in alphas.items()]
+# turning the dictionary into a list and sorting for highest count
+def dict_to_list(alpha_characters):
+    character_list = [{"character": character, "count": count} for character, count in alpha_characters.items()]
     character_list.sort(key=sort_on, reverse=True)
     return character_list
 
-def print_report(book_path, word_count, character_list):
-    print(f"|--- Start of report for {book_path} ---|")
-    print(f"Total word count: {word_count}\n")
-    for dictionary in character_list:
-        print(f"The character '{dictionary['character']}' was found {dictionary['count']} times\n")
-    print("|--- End of report. Thanks for using Bookbot v0.1 ---|")
+# formats and outputs the report as a text file
+def return_report(file_path, word_count, character_list):
+    with open("reports/report.txt", "w") as f:
+                f.write(f"|--- Start of report for {file_path} ---|\n")
+                f.write(f"Total word count: {word_count}\n")
+                for dictionary in character_list:
+                    f.write(f"The character '{dictionary['character']}' was found {dictionary['count']} times\n")
+                f.write("|--- End of report. Thanks for using Bookbot v0.1 ---|")
 
 main()
